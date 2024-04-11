355fb3039b9d0914d5d4128c06d41562249d6cef
Sachin Gurjar <98685642+SachinCoder1@users.noreply.github.com>
2024-04-11 09:52:14 +0530

    arbitrum sepolia setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const ARBITRUM_PRIVATE_KEY = process.env.ARBITRUM_SEPOLIA_TESTNET_PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+    },
+    arbitrumSepolia: {
+      url: "https://sepolia-rollup.arbitrum.io/rpc",
+      accounts: [ARBITRUM_PRIVATE_KEY]
+    }
+  },
+
 };
